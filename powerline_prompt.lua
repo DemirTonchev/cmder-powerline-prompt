8a4c0511d648ba8db9bcf7610b1ef1ee4149581c
DT <tonchev.demir@gmail.com>
2020-12-28 18:06:30 +0200

    show conda env
    https://github.com/AmrEldib/cmder-powerline-prompt/issues/5

--- powerline_prompt.lua
+++ powerline_prompt.lua
@@ -82,5 +82,29 @@ local function addAddonSegment()
     addSegment(segment.text, segment.textColor, segment.fillColor)
 end 
 
+
+---------------- show conda venv 
+
+local function get_py_env(env_var)
+    env = clink.get_env(env_var)
+    if env then
+        return env
+    end
+
+    return false
+end
+
+local function get_conda_env_filter()
+    local env = get_py_env("CONDA_DEFAULT_ENV")
+
+    if env then
+        env = addTextWithColor("( ", env.." ) ", ansiFgClrWhite  , ansiBgClrBlack)
+        clink.prompt.value = string.gsub(clink.prompt.value, plc_prompt_lambSymbol, env..plc_prompt_lambSymbol..)
+    end
+
+    return false
+end
+
 -- Register this addon with Clink
+clink.prompt.register_filter(get_conda_env_filter, 100)
 clink.prompt.register_filter(addAddonSegment, 55)
